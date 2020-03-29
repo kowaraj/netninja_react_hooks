e5de2bb04013ef99cadad29d1ec7a5cf1a898a8e
kowaraj <kowaraj4stuff@gmail.com>
2020-03-29 13:18:02 +0200

    Fix the bug

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -19,7 +19,7 @@ class Navbar extends Component {
                         </ul>
                     </nav>
                 )
-            }} </ThemeContext.Consumer>
+            }}</ThemeContext.Consumer>
         )
     }
 }
