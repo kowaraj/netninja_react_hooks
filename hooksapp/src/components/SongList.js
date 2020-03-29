f744eca923011219981133a8e51bcf246a3e8e9d
kowaraj <kowaraj4stuff@gmail.com>
2020-03-29 16:25:26 +0200

    Add another app to play with React.Hooks

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SongList = () => {
+    return (  
+        <div className='song-list'>
+            <ul>
+                <li>song one</li>
+                <li>song two</li>
+                <li>song three</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default SongList;
