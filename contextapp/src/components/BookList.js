be244d082611485ac2504048b70be4cbac26c3d5
kowaraj <kowaraj4stuff@gmail.com>
2020-03-29 19:21:16 +0200

    How to use HOOKS with the CONTEXT (non-working example)

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
+class BookList-OLD extends Component {
+    static contextType = ThemeContext; // <---- cannot be used inside a Functional Component
     render() { 
         const { isLightTheme, light, dark } = this.context;
         const theme = isLightTheme ? light : dark;
@@ -17,7 +17,22 @@ class BookList extends Component {
         );
     }
 }
+
+const BookList-NEW = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <div className="book-list"  style={{background: theme.bg, color: theme.syntax }}>
+            <ul>
+            <li  style={{background: theme.ui }}>book one</li>
+            <li  style={{background: theme.ui }}>book two</li>
+            <li  style={{background: theme.ui }}>book three</li>
+            </ul>
+        </div>
+    );
+}
  
+
 export default BookList;
 
 
