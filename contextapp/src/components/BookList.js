bd6d64b9f5095574cfde9b0ee07d8894f9f0d1e0
kowaraj <kowaraj4stuff@gmail.com>
2020-03-29 12:24:13 +0200

    first release

---
+++ contextapp/src/components/BookList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div className="book-list">
+                <ul>
+                <li>book one</li>
+                <li>book two</li>
+                <li>book three</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default BookList;
+
+
+
